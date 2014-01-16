4f34b8827ef3214aa0090828b3aef79eaa1e5c4d
Anders Nissen <ani@marimatech.com>
2014-01-16 21:11:36 +0100

    Initial commit

---
+++ src/Game.js
@@ -0,0 +1,295 @@
+
+BasicGame.Game = function (game) {
+  //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+  /*
+  this.game;    //  a reference to the currently running game
+  this.add;   //  used to add sprites, text, groups, etc
+  this.camera;  //  a reference to the game camera
+  this.cache;   //  the game cache
+  this.input;   //  the global input manager (you can access this.input.keyboard, this.input.mouse, as well from it)
+  this.load;    //  for preloading assets
+  this.math;    //  lots of useful common math operations
+  this.sound;   //  the sound manager - add a sound, play one, set-up markers, etc
+  this.stage;   //  the game stage
+  this.time;    //  the clock
+  this.tweens;  //  the tween manager
+  this.world;   //  the game world
+  this.particles; //  the particle manager
+  this.physics; //  the physics manager
+  this.rnd;   //  the repeatable random number generator
+  */
+  //  You can use any of these from any function within this State.
+  //  But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
+  this.CARS = 2;
+  this.PATIENTS = 6;
+  this.HOSPITALS = 1;
+
+  //this.cars;
+  //this.patients;
+  //this.hospitals;
+
+  this.maxTimeOut = 60000;
+
+  this.selected = null;
+
+  //this.loadingText;
+  //this.scoreText;
+
+  this.score = 0;
+
+  //this.popupTexts;
+  this.tileSize = 50;
+  this.carDragged = null;
+};
+
+BasicGame.Game.prototype = {
+  create: function () {
+    //game.world.setBounds(50, 50, game.width - 50, game.height - 50);
+    this.world.setBounds(0, 0, this.game.width, this.game.height);
+
+    //  For browsers that support it, this keeps our pixel art looking crisp
+    // This only works when you use Phaser.CANVAS as the renderer
+    Phaser.Canvas.setSmoothingEnabled(this.game.context, false);
+    var map = this.add.sprite(this.world.centerX, this.world.centerY, 'map');
+    map.anchor.setTo(0.5, 0.5);
+    map.scale.setTo(50, 50);
+
+    this.hospitals = this.add.group();
+    for (var h = 0; h < this.HOSPITALS; h++) {
+      // var hospital = hospitals.create(this.world.randomX, this.world.randomY, 'hospital');
+      var hospital = this.hospitals.create(this.world.centerX, this.world.centerY, 'hospital');
+      hospital.anchor.setTo(0.5, 0.5);
+      hospital.inputEnabled = true;
+      hospital.events.onInputDown.add(this.toHospital, this);
+    }
+
+    this.patients = this.add.group();
+    for (var p = 0; p < this.PATIENTS; p++) {
+      var patient = this.patients.create(this.snapX(this.world.randomX), this.snapY(this.world.randomY), 'patient');
+      patient.anchor.setTo(0.5, 0.5);
+      patient.inputEnabled = true;
+      patient.events.onInputDown.add(this.carDispatched, this);
+
+      patient.countDown = this.rnd.realInRange(this.maxTimeOut / 5, this.maxTimeOut);
+      patient.countTween = this.add.tween(patient);
+      patient.countTween.onComplete.add(this.patientDies, this);
+      patient.countTween.to({ countDown: 0 }, patient.countDown, Phaser.Easing.Quadratic.Out, true);
+
+      this.add.tween(patient)
+        .to({ rotation: -Math.PI / 6 }, 1000, Phaser.Easing.Quadratic.InOut)
+        .to({ rotation: Math.PI / 6 }, 1000, Phaser.Easing.Quadratic.InOut)
+        .loop()
+        .start();
+    }
+
+    this.cars = this.add.group();
+    for (var c = 0; c < this.CARS; c++) {
+      var car = this.cars.create(this.world.randomX, this.world.randomY, 'car');
+      car.anchor.setTo(0.5, 0.5);
+      car.inputEnabled = true;
+      car.events.onInputDown.add(this.selectCar, this);
+      car.input.enableDrag(false, true);
+      car.input.setDragLock(false, false);
+      car.events.onDragStart.add(this.startDragCar, this);
+      car.events.onDragStop.add(this.endDragCar, this);
+      car.color = this.randomColor();
+      car.dragging = false;
+      car.movePoints = [];
+    }
+
+    this.popupTexts = this.add.group();
+
+    this.scoreText = this.add.text(16, 16, '0 patients saved', { font: "16pt Courier", fill: "#ee0000", stroke: "#ffffff", strokeThickness: 2 });
+
+    //loadingText.destroy();
+    //this.stage.scale.startFullScreen();
+  },
+
+  render: function() {
+    var ctx = this.game.context;
+
+    if (this.selected) {
+      ctx.lineWidth = 4;
+      ctx.strokeStyle = this.selected.color;
+      ctx.beginPath();
+      ctx.arc(this.selected.center.x, this.selected.center.y, 30, 0, Math.PI * 2);
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    this.cars.forEach(function(car) {
+      if (car.movePoints.length > 0) {
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = car.color;
+        ctx.beginPath();
+        ctx.moveTo(car.movePoints[0][0], car.movePoints[0][1]);
+        for (var i = 1; i < car.movePoints.length; i++) {
+          ctx.lineTo(car.movePoints[i][0], car.movePoints[i][1]);
+        }
+        ctx.stroke();
+        ctx.closePath();
+      }
+
+      if (!car.dest) return;
+      ctx.lineWidth = 4;
+      ctx.strokeStyle = car.color;
+      ctx.beginPath();
+      ctx.moveTo(car.center.x, car.center.y);
+      ctx.lineTo(car.dest.center.x, car.dest.center.y);
+      ctx.stroke();
+      ctx.closePath();
+
+      if (car.full) {
+        ctx.beginPath();
+        ctx.arc(car.center.x, car.center.y, 30, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fillStyle = 'rgba(255, 0, 0, 0.2)';
+        ctx.fill();
+      }
+    });
+
+    var me = this;
+    this.patients.forEach(function(patient) {
+      ctx.beginPath();
+      var timePercentage = (patient.countDown / me.maxTimeOut);
+      ctx.arc(patient.center.x, patient.center.y, 30, -Math.PI / 2, -Math.PI / 2 + (Math.PI * 2) * timePercentage, false); // 60000 is max count down for now
+      ctx.lineWidth = 6;
+
+      // from: 0, 255, 0
+      // to: 255, 0, 0
+      ctx.strokeStyle = 'rgb(' + Math.round(255 - 255 * timePercentage) + ', ' + Math.round(255 * timePercentage) + ', 0)';
+      ctx.stroke();
+    });
+  },
+
+  update: function () {
+    if (this.carDragged) {
+      var tileSize = 50;
+      var snapX = this.input.x; // Math.floor(this.input.x / tileSize) * tileSize + tileSize / 2;
+      var snapY = this.input.y; //Math.floor(this.input.y / tileSize) * tileSize;
+      this.carDragged.movePoints.push([snapX, snapY]);
+    }
+  },
+
+  quitGame: function (pointer) {
+    //  Here you should destroy anything you no longer need.
+    //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
+
+    //  Then let's go back to the main menu.
+    this.game.state.start('MainMenu');
+  },
+
+  popupText: function(x, y, text, color) {
+    var t = this.popupTexts.add(this.add.text(x, y, text, { font: "20px Arial", fill: color, align: "center" }));
+    t.anchor.setTo(0.5, 0.5);
+    t.alpha = 0.0;
+
+    this.add.tween(t)
+      .to({ alpha: 1, y: y - 30 }, 800, Phaser.Easing.Elastic.Out)
+      .to({ alpha: 0, y: y }, 800, Phaser.Easing.Cubic.InOut)
+      .start();
+      
+    this.add.tween(t)
+      .to({}, 1700)
+      .start()
+      .onComplete.add(function() { console.log('destroyed'); t.destroy(); });
+  },
+
+  selectCar: function(sprite) {
+    this.selected = sprite;
+  },
+
+  carDispatched: function(dest) {
+    if (!this.selected || this.selected.full) return;
+
+    if (this.selected.moveTween) this.selected.moveTween.stop();
+    this.selected.moveTween = this.add.tween(this.selected);
+    this.selected.moveTween.onComplete.add(this.carArrivedAtPatient, this);
+    var me = this;
+    dest.events.onKilled.add((function(car) {
+      return function() {
+        me.patientForCarDied(dest, car);
+      };
+    })(this.selected), this);
+    this.selected.dest = dest;
+    var distance = this.selected.center.distance(dest.center);
+    this.selected.moveTween.to({ x: dest.x, y: dest.y }, distance * 10, Phaser.Easing.Quadratic.InOut, true);
+
+    this.selected.rotation = this.physics.angleBetween(this.selected, this.selected.dest) + Math.PI / 2;
+  },
+
+  patientForCarDied: function(patient, car) {
+    car.moveTween.stop();
+    car.full = false;
+    car.patient = false;
+    car.dest = null;
+  },
+
+  carArrivedAtPatient: function(car) {
+    car.full = true;
+    car.patient = car.dest;
+    car.patient.center = car.center;
+    car.patient.visible = false;
+  },
+
+  carArrivedAtHospital: function(car) {
+    if (car.full) {
+      this.popupText(car.patient.center.x, car.patient.center.y, 'A patient was saved', 'green');
+      this.score++;
+      this.scoreText.content = this.score + ' patients saved';
+      car.patient.countTween.stop();
+      car.patient.destroy();
+
+      this.add.tween(car.dest)
+        .to({ rotation: this.rnd.normal() * 3 }, 500, Phaser.Easing.Cubic.InOut)
+        .to({ rotation: 0 }, 500, Phaser.Easing.Cubic.InOut)
+        .start();
+    }
+    car.full = false;
+    car.dest = null;
+  },
+
+  toHospital: function(hospital) {
+    if (!this.selected) return;
+
+    if (this.selected.moveTween) this.selected.moveTween.stop();
+    this.selected.moveTween = this.add.tween(this.selected);
+    this.selected.moveTween.onComplete.add(this.carArrivedAtHospital, this);
+    this.selected.dest = hospital;
+    var distance = this.selected.center.distance(hospital.center);
+    this.selected.moveTween.to({ x: hospital.center.x, y: hospital.center.y }, distance * 20, Phaser.Easing.Quadratic.InOut, true);
+
+    this.selected.rotation = this.physics.angleBetween(this.selected, this.selected.dest) + Math.PI / 2;
+  },
+
+  patientDies: function(patient) {
+    this.popupText(patient.center.x, patient.center.y, 'A patient has died!', 'red');
+    patient.kill();
+  },
+
+  randomColor: function() {
+    var me = this;
+    var rndColorInt = function() { return me.rnd.integerInRange(0, 255); };
+    return 'rgb(' + rndColorInt() + ',' + rndColorInt() + ',' + rndColorInt() + ')';
+  },
+
+  snapX: function(x) {
+    return Math.floor(x / this.tileSize) * this.tileSize; //  + tileSize / 2
+  },
+
+  snapY: function(y) {
+    return Math.floor(y / this.tileSize) * this.tileSize;
+  },
+
+  startDragCar: function(car) {
+    car.dragging = true;
+    car.movePoints = [];
+    this.carDragged = car;
+  },
+
+  endDragCar: function(car) {
+    car.dragging = false;
+    this.carDragged = null;
+  }
+
+};
---
+++ src/Preloader.js
@@ -0,0 +1,66 @@
+
+BasicGame.Preloader = function (game) {
+
+  this.background = null;
+  this.preloadBar = null;
+
+  this.ready = false;
+
+};
+
+BasicGame.Preloader.prototype = {
+
+  preload: function () {
+    //  These are the assets we loaded in Boot.js
+    //  A nice sparkly background and a loading progress bar
+    this.background = this.add.tileSprite(0, 0, 1024, 768, 'preloaderBackground');
+    this.background.anchor.setTo(0.5, 0.5);
+    this.preloadBar = this.add.sprite(256, 512, 'preloaderBar');
+    //this.preloadBar.anchor.setTo(0.5, 0.5);
+
+    //  This sets the preloadBar sprite as a loader sprite.
+    //  What that does is automatically crop the sprite from 0 to full-width as the files below are loaded in.
+    this.load.setPreloadSprite(this.preloadBar);
+
+    //  Here we load the rest of the assets our game needs.
+    //  As this is just a Project Template I've not provided these assets, the lines below won't work as the files themselves will 404, they are just an example of use.
+    this.load.image('titlepage', 'assets/images/title.png');
+    this.load.atlas('playButton', 'assets/images/button_texture_atlas.png', 'assets/images/button_texture_atlas.json');
+    this.load.audio('titleMusic', ['assets/music/main_menu.mp3']);
+    this.load.bitmapFont('desyrel', 'assets/fonts/desyrel-pink.png', 'assets/fonts/desyrel-pink.xml');
+    //this.load.bitmapFont('caslon', 'fonts/caslon.png', 'fonts/caslon.xml');
+    //  + lots of other required assets here
+    this.stage.backgroundColor = '#440055';
+
+    loadingText = this.add.text(512, 570, 'Loading', { font: "30pt Courier", fill: "#ee0000", stroke: "#ffffff", strokeThickness: 5, align: 'center' });
+    loadingText.anchor.setTo(0.5, 0.5);
+
+    this.load.image('map', 'assets/images/map.png');
+    this.load.image('car', 'assets/images/car.png');
+    this.load.image('patient', 'assets/images/firstaid.png');
+    this.load.image('hospital', 'assets/images/spinObj_02.png');
+  },
+
+  create: function () {
+    //  Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
+    this.preloadBar.cropEnabled = false;
+  },
+
+  update: function () {
+    //  You don't actually need to do this, but I find it gives a much smoother game experience.
+    //  Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
+    //  You can jump right into the menu if you want and still play the music, but you'll have a few
+    //  seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
+    //  it's best to wait for it to decode here first, then carry on.
+    
+    //  If you don't have any music in your game then put the game.state.start line into the create function and delete
+    //  the update function completely.
+    
+    if (this.cache.isSoundDecoded('titleMusic') && this.ready === false) {
+      this.ready = true;
+      this.game.state.start('MainMenu');
+    }
+
+  }
+
+};
